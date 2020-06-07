0d95850ed840f07605b5a3e1c2c2649209ccd3e9
Luffy <ogistdipen@outlook.com>
2020-06-07 20:59:57 +0200

    removed lock

---
+++ build/precache-manifest.cc8f227c6e61d3dbcd79fdde3da4a994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289d066ce470c33eada55fb2be09a10d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c2f1410a7883f88a454",
+    "url": "/static/css/2.934bc327.chunk.css"
+  },
+  {
+    "revision": "e7a8978e8129cdfe0333",
+    "url": "/static/css/main.c3d270a3.chunk.css"
+  },
+  {
+    "revision": "8c2f1410a7883f88a454",
+    "url": "/static/js/2.e07120ca.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.e07120ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a8978e8129cdfe0333",
+    "url": "/static/js/main.50da87bc.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
